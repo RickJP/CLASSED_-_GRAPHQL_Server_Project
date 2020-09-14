08e2f45504b647bdaf856dd80d7d0ca91aff7fd8
rickjp <3741326+RickJP@users.noreply.github.com>
2020-09-14 11:34:23 +0900

    ADD - finished likes & comments functionality

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const {ApolloServer} = require('apollo-server');
+const {ApolloServer, PubSub} = require('apollo-server');
 // const gql = require('graphql-tag');
 const mongoose = require('mongoose');
-const typeDefs = require('./graphql/typeDefs');
 
+const typeDefs = require('./graphql/typeDefs');
 const {MONGO_DB} = require('./config');
 const resolvers = require('./graphql/resolvers');
+
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req}) => ({req}),
+  context: ({req}) => ({req, pubsub}),
 });
 
 mongoose
